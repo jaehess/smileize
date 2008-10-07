4397e55b3b058cb4853dbad74baf27cb359bc89f
Jae <jae.hess@gmail.com>
2008-10-07 16:33:49 -0400

    Fixing error in install.rb

--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
 require 'fileutils'
 target = File.join(RAILS_ROOT, 'public', 'images', 'smileys')
 mkdir_p(target)
-FileUtils.cp_r File.join(File.dirname(__FILE__), "icons", "*"), target
+FileUtils.cp_r File.join(File.dirname(__FILE__), "icons"), target
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
